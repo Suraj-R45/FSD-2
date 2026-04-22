b918cfee2b35b3a904b4d0a7c33dba379f39ffa9
Suraj-R45 <parmarsuraj8115@gmail.com>
2026-04-22 19:10:01 +0530

    updated experiment_20

--- Experiment_20/backend/app.py
+++ Experiment_20/backend/app.py
@@ -7,10 +7,14 @@ def create_app():
     # register blueprint
     app.register_blueprint(student_bp)
 
+    @app.route("/")
+    def home():
+        return {"message": "Backend Server is running"}, 200
+
     return app
 
 # IMPORTANT: expose app for pytest and CI
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
